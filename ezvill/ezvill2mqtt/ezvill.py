27f4a178d31415007a3092ecd2d4be134c11240e
ohoohoo7 <63686427+ohoohoo7@users.noreply.github.com>
2020-04-19 07:46:05 +0900

    Update ezvill.py

--- ezvill/ezvill2mqtt/ezvill.py
+++ ezvill/ezvill2mqtt/ezvill.py
@@ -66,7 +66,7 @@ def do_work(config, device_list):
                 log('[SIGNAL] receved: {}'.format(data))
             if len(data) > 1:
                 for kk in range(len(data)):
-                    device_seperator = data[kk][2:6]
+                    device_seperator = data[kk][seperator_startnum-len(data_prefix)-1:seperator_length+seperator_startnum-len(data_prefix)-1]
                     if device_seperator in seperator_list:
                         device_name = seperator_list[device_seperator]
                         num = DEVICE_LISTS[device_name]['Number']
